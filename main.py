be03766464c843d3d43121c20b0ac16619852e20
Bayegle <65605885+Bayegle@users.noreply.github.com>
2023-10-25 00:52:49 +0100

    Create main.py
    Main program for testing the game

---
+++ main.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    word_list = ["bananas", "apples", "oranges", "pineaples", "watermelon"]
+    #word_list = ["pineaples"]
+    play_game(word_list)
+    print(" GOOD BYE ")
